5e4fd2970311b30d1f33a87ba1de0c2730c4e76c
mercure84 <julien.marcesse@gmail.com>
2019-02-18 14:03:08 +0100

    démarrage implémentation bean et DB avec jdbc

--- src/main/java/com/escaladeP6/beans/Membre.java
+++ src/main/java/com/escaladeP6/beans/Membre.java
@@ -1,4 +1,19 @@
 package com.escaladeP6.beans;
 
+import java.util.Date;
+
 public class Membre {
+    private String nom;
+    private String prenom;
+    private String pseudo;
+    private String password;
+    private Date dateNaissance;
+    private String email;
+    private int telMobile;
+    private Date dateInscription;
+    private String role;
+    private String biographique;
+    private String ville;
+    private boolean valide;
+
 }
