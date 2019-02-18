34c4cc7be03a32b6afacc806c7c1fee86a4e7d3e
mercure84 <julien.marcesse@gmail.com>
2019-02-18 19:16:55 +0100

    début reconstruction du formulaire d'enregistrement avec injection du thymleaf

--- src/main/java/com/escaladeP6/controllers/SignUpController.java
+++ src/main/java/com/escaladeP6/controllers/SignUpController.java
@@ -1,15 +1,27 @@
 package com.escaladeP6.controllers;
 
+import com.escaladeP6.beans.Membre;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class SignUpController {
 
     @GetMapping("/signUp")
-    public String index (Model model){
+    public String senregistrerForm (Model model){
+        model.addAttribute("membre", new Membre());
         return "signUp";
+    }
+
+    public String senregistrerSubmit(@ModelAttribute Membre membre){
+
+        System.out.println("les données du formulaires :");
+        System.out.println(membre.getNom() + " " + membre.getPrenom() + " " + membre.getPseudo());
+
+        return "index";
+
     }
 
 }
