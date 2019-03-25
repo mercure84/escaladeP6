09690512b6e5a76e5beec243308a791870edf832
mercure84 <julien.marcesse@gmail.com>
2019-03-25 12:53:19 +0100

    gestion du conflit sur l'upload des fichiers : il n'y a plus d'erreur en cas d'absence d'upload + refactoring creer / editer - reste à gérer l'update

--- src/main/java/com/escaladeP6/controllers/EditerTopoController.java
+++ src/main/java/com/escaladeP6/controllers/EditerTopoController.java
@@ -4,21 +4,37 @@ package com.escaladeP6.controllers;
 import com.escaladeP6.DAO.MembreRepository;
 import com.escaladeP6.DAO.TopoRepository;
 import com.escaladeP6.beans.Departement;
+import com.escaladeP6.beans.Membre;
 import com.escaladeP6.beans.Topo;
+import com.escaladeP6.storage.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.security.Principal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 @Controller
 public class EditerTopoController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public EditerTopoController(StorageService storageService) {
+        this.storageService = storageService;
+    }
 
     @Autowired
     TopoRepository repositoryTopo;
@@ -41,21 +57,75 @@ public class EditerTopoController {
         for (Departement dept : Departement.values()){
             listeNumDpt.add(dept.getNumDpt());
         }
-        model.addAttribute("topo", new Topo());
         model.addAttribute("listeDept",listeNumDpt);
 
 
-        //recherche du Topo à éditer !
-        Topo editedTopo = repositoryTopo.findTopoById(Integer.parseInt(topoId));
+        //recherche du Topo à éditer si le membre à cliquer sur Editer
+        String creer = "creer";
 
-        model.addAttribute("editedTopo", editedTopo);
+
+        if (!topoId.equals(creer)){
+        Topo editedTopo = repositoryTopo.findTopoById(Integer.parseInt(topoId));
+        model.addAttribute("topo", editedTopo);
         System.out.println(editedTopo);
+        }else
+            {
+            //sinon on envoie un objet nouveau topo :)
+            model.addAttribute("topo", new Topo());
+
+
+        }
 
         return "topoEditer";
     }
 
-//    @PostMapping(value="topoEditer")
-//    public
+    @PostMapping("/topoEditer")
+    public String publicationSubmit (@RequestParam("file") MultipartFile file, @ModelAttribute Topo topo, Principal principal, Model model) throws SQLException {
+        // SELECTION DU MEMBRE QUI POSTE LE TOPO
 
 
+
+        //chargement des paramètres du membres
+        User loginedUser = (User) ((Authentication) principal).getPrincipal();
+        String pseudo = loginedUser.getUsername();
+        int idMembre = repositoryMembre.findMembreByPseudo(pseudo).getId();
+        Membre membreEditeur = repositoryMembre.findMembreById(idMembre);
+
+        //        requete des topos de l'utilisateur
+        model.addAttribute("mesTopos", repositoryTopo.findToposByMembreId(idMembre));
+
+
+        repositoryTopo.save(new Topo(topo.getNom(), topo.getDescription(), topo.getDepartement(), topo.getDifficulte(), topo.getNbVoies(), topo.isDisponible(), topo.isValide(), membreEditeur));
+
+
+        // TRAITEMENT DU STOCKAGE DU FICHIER SI L'UTILISATEUR PROPOSER UN UPLOAD
+        if(!file.isEmpty()){
+
+        String nomInitialFichier = file.getOriginalFilename();
+        storageService.store((file));
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/escap6", "postgres", "dionae1984");
+
+        try (PreparedStatement ps = conn.prepareStatement("UPDATE topo SET nom_fichier=?, fichier=? WHERE nom=?")) {
+
+
+            //traitement du fichier
+            System.out.println("le nom du fichier est : " + nomInitialFichier);
+            File fileTopo = new File("fichiers/" + nomInitialFichier);
+            try {
+                FileInputStream fis = new FileInputStream(fileTopo);
+                ps.setString(1, topo.getNom());
+                ps.setBinaryStream(2, fis, fileTopo.length());
+                ps.setString(3, topo.getNom());
+                ps.executeUpdate();
+                ps.close();
+                fis.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }}
+
+        return "/topoGerer";
+    }
 }
--- src/main/java/com/escaladeP6/controllers/GererTopoController.java
+++ src/main/java/com/escaladeP6/controllers/GererTopoController.java
@@ -3,39 +3,23 @@ package com.escaladeP6.controllers;
 import com.escaladeP6.DAO.MembreRepository;
 import com.escaladeP6.DAO.TopoRepository;
 import com.escaladeP6.beans.Departement;
-import com.escaladeP6.beans.Membre;
 import com.escaladeP6.beans.Topo;
 
-import com.escaladeP6.storage.StorageService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+
+
 import java.security.Principal;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.ArrayList;
 
 @Controller
 public class GererTopoController {
-
-    private final StorageService storageService;
-
-    @Autowired
-    public GererTopoController(StorageService storageService) {
-        this.storageService = storageService;
-    }
 
      @Autowired
     TopoRepository repositoryTopo;
@@ -53,12 +37,6 @@ public class GererTopoController {
         String pseudo = loginedUser.getUsername();
         int idMembre = repositoryMembre.findMembreByPseudo(pseudo).getId();
 
-        ArrayList<Integer> listeNumDpt = new ArrayList<Integer>();
-        for (Departement dept : Departement.values()){
-            listeNumDpt.add(dept.getNumDpt());
-        }
-        model.addAttribute("topo", new Topo());
-        model.addAttribute("listeDept",listeNumDpt);
 
 //        requete des topos de l'utilisateur
         model.addAttribute("mesTopos", repositoryTopo.findToposByMembreId(idMembre));
@@ -67,53 +45,7 @@ public class GererTopoController {
 
     }
 
-    @PostMapping("/topoGerer")
-    public String publicationSubmit (@RequestParam("file") MultipartFile file, @ModelAttribute Topo topo, Principal principal, Model model) throws SQLException {
-        // SELECTION DU MEMBRE QUI POSTE LE TOPO
 
-
-
-        //chargement des paramètres du membres
-        User loginedUser = (User) ((Authentication) principal).getPrincipal();
-        String pseudo = loginedUser.getUsername();
-        int idMembre = repositoryMembre.findMembreByPseudo(pseudo).getId();
-        Membre membreEditeur = repositoryMembre.findMembreById(idMembre);
-
-        //        requete des topos de l'utilisateur
-        model.addAttribute("mesTopos", repositoryTopo.findToposByMembreId(idMembre));
-
-
-        repositoryTopo.save(new Topo(topo.getNom(), topo.getDescription(), topo.getDepartement(), topo.getDifficulte(), topo.getNbVoies(), topo.isDisponible(), topo.isValide(), membreEditeur));
-
-
-    // TRAITEMENT DU STOCKAGE DU FICHIER
-        String nomInitialFichier = file.getOriginalFilename();
-        storageService.store((file));
-        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/escap6", "postgres", "dionae1984");
-
-        try (PreparedStatement ps = conn.prepareStatement("UPDATE topo SET nom_fichier=?, fichier=? WHERE nom=?")) {
-
-
-            //traitement du fichier
-            System.out.println("le nom du fichier est : " + nomInitialFichier);
-            File fileTopo = new File("fichiers/" + nomInitialFichier);
-            try {
-                FileInputStream fis = new FileInputStream(fileTopo);
-                ps.setString(1, topo.getNom());
-                ps.setBinaryStream(2, fis, fileTopo.length());
-                ps.setString(3, topo.getNom());
-                ps.executeUpdate();
-                ps.close();
-                fis.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return "/topoGerer";
-    }
 
 
 
