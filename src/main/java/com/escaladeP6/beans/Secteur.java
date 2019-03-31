f76e95d426b86f39204c4672a97f061dce25fecd
mercure84 <julien.marcesse@gmail.com>
2019-03-31 19:27:10 +0200

    modif accueil et début implémentation du bean des voies

--- src/main/java/com/escaladeP6/beans/Secteur.java
+++
@@ -1,7 +0,0 @@
-package com.escaladeP6.beans;
-
-public class Secteur {
-
-    private String nom;
-    private Site site;
-}
--- src/main/java/com/escaladeP6/beans/Voie.java
+++ src/main/java/com/escaladeP6/beans/Voie.java
@@ -1,6 +1,30 @@
 package com.escaladeP6.beans;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="voie")
 public class Voie {
-    private String nom, difficulte;
-    private Secteur secteur;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="difficulte")
+    private int difficulte;
+
+    @Column(name="nom")
+    private String nom;
+
+    @Column(name="nom_site")
+    private String nom_site;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="topo_id")
+    private Topo topo;
+
+    @Column(name="longueur")
+    private int longueur;
 }
